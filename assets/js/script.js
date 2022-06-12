f66ad34c65aa75862bc0d0c4846fa57bfc24e1d2
millerd <millerdicarlo@gmail.com>
2022-06-12 18:17:08 -0400

    added event listener and clear ability

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,19 @@ function weatherDash() {
 
 
 
+// Event listener and clear
+searchEl.addEventListener("click",function() {
+    const searchTerm = inputEl.value;
+    getWeather(searchTerm);
+    searchHistory.push(searchTerm);
+    localStorage.setItem("search",JSON.stringify(searchHistory));
+    renderSearchHistory();
+})
 
+clearEl.addEventListener("click",function() {
+    searchHistory = [];
+    renderSearchHistory();
+})
 
 
 }
