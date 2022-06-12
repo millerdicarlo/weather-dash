c3f4a9d9e2f90473a8bd3ea038543059e32ebc08
millerd <millerdicarlo@gmail.com>
2022-06-12 18:19:35 -0400

    initial variables added to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 function weatherDash() {
-
+    var inputEl = document.getElementById("city-input");
+    var searchEl = document.getElementById("search-button");
+    var clearEl = document.getElementById("clear-history");
+    var nameEl = document.getElementById("city-name");
+    var currentPicEl = document.getElementById("current-pic");
+    var currentTempEl = document.getElementById("temperature");
+    var currentHumidityEl = document.getElementById("humidity");4
+    var currentWindEl = document.getElementById("wind-speed");
+    var currentUVEl = document.getElementById("UV-index");
+    var historyEl = document.getElementById("history");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
 
 
 
